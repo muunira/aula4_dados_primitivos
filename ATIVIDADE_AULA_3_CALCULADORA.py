56e50e0b6a6ed29cea87b57670dfbe8ddf04d399
Munira Majdoub <munira_majdoub@hotmail.com>
2024-10-18 17:32:55 -0300

    Add files via upload

---
+++ ATIVIDADE_AULA_3_CALCULADORA.py
@@ -0,0 +1,15 @@
+# ATIVIDADE AULA 3 - CALCULADORA:
+
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite um número: '))
+soma = n1 + n2
+subtração = n1 - n2
+multiplicação = n1 * n2
+divisão = n1 / n2
+
+print(f'''
+Soma - {soma}
+Subtração - {subtração}
+Multiplicação - {multiplicação}
+Divisão - {divisão}
+''')
