e1da778c27ba29787491cf89d9336ea74783ff5b
Munira Majdoub <munira_majdoub@hotmail.com>
2024-10-18 16:00:21 -0300

    Add files via upload

---
+++ ATIVIDADE_AULA_4.py
@@ -0,0 +1,47 @@
+# Tipos de dados primitivos:
+
+# declarações semânticas (Snake Case que é o underline):
+
+user_name = 'Eloysa'
+
+# strings = textos (os 3 são diferentes):
+
+nome = 'Carlos'
+NOME = 'Elo'
+Nome = 'Yasmin'
+
+# floats = números reais (com pontos flutuantes):
+
+real = 24.9
+real_2 = 0.
+real_3 = .0
+
+# integer (int) = números inteiros (sem pontos flutuantes):
+
+numero_inteiro1 = 10
+numero_inteiro2 = -10
+numer_inteiro3 = 0
+
+# bool (boleanos) = dados lógicos (True or False):
+
+verdadeiro = True
+falso = False
+
+# Função = ação:
+
+numero = 10
+text = 'hoje está nublado'
+boleana = True
+real = 5.90
+
+tipo = type(numero)
+#tipo2 = type(text)
+#tipo3 = type(boleana)
+#tipo4 = type(real)
+#tipo5 = type(inteiro_em_real)
+
+
+inteiro_em_real = float(numero)
+texto_em_bool = float(text)
+
+
