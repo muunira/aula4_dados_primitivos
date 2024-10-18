df01581886574c27bcfacd9c3dadeddb36cfdd6c
Munira Majdoub <munira_majdoub@hotmail.com>
2024-10-18 18:01:45 -0300

    Add files via upload

---
+++ ATIVIDADE_ARITMETICOS.py
@@ -0,0 +1,31 @@
+# == -> É UMA COMPARAÇÃO:
+
+print(10 == 10)
+#verdadeiro
+
+# > -> MAIOR
+
+print(10>2)
+#verdadeiro
+
+# < -> MENOR
+print(10<2)
+#falso
+
+# >= -> MAIOR OU IGUAL
+print(10>=2)
+#verdadeiro
+
+# <= -> MENOR OU IGUAL
+print(30>=50)
+#falso
+
+# ARITMÉTICOS:
+
+# // divisão sem o ponto flutuante
+print(1.5 // 2)
+#traz o primeiro número do resultado
+
+# % -> módulo
+print(20%2)
+#traz o resto da conta de divisão
